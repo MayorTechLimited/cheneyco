226c13a53fe8bdfc87a8a3fc40acfbc8bff5f8d8
William Mayor <mail@mayortech.co.uk>
2022-04-29 20:41:26 +0100

    Add colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     theme: {
         extend: {
             colors: {
+                "brand-yellow": "#FCA311",
+                "brand-blue": "#14213D",
+                "brand-gray": "#E5E5E5"
             }
         },
     },
