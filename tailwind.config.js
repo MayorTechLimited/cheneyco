d804d572f3fa97fd4e8a277dbaeb37d205a0d263
William Mayor <mail@mayortech.co.uk>
2022-05-03 11:10:31 +0100

    Style tweaks from Stu

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
         extend: {
             colors: {
                 "brand-yellow": "#FCA311",
-                "brand-blue": "#14213D",
                 "brand-gray": "#E5E5E5"
             }
         },
