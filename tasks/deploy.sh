97ed5492b96a5301aeda571115fd5e22162a62ff
William Mayor <mail@mayortech.co.uk>
2023-04-06 11:57:15 +0100

    Add resources css file

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -7,6 +7,7 @@ rm -rf dist
 staticjinja build --outpath ./dist
 cp -r images dist/
 tailwindcss -i ./styles.css -o ./dist/styles.css --minify
+tailwindcss -i ./resources.css -o ./dist/resources.css --minify
 
 ghp-import dist --branch=main --cname=cheneyco.mayortech.co.uk
 
